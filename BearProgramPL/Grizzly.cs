2c0bc4289e22114c7d9fc46002f5ce75409e0747
Philippe Lumpkin <phiglump@ut.utm.edu>
2019-02-05 16:03:42 -0600

    Add project files.

---
+++ BearProgramPL/Grizzly.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearProgramPL
+{
+    class Grizzly : Bear
+    {
+        void Bear.hibernate()
+        {
+            string brief = "Hibernating bear";
+            Console.WriteLine(brief);
+        }
+
+        void Bear.maul()
+        {
+            string brief = "Mauling Bear";
+            Console.WriteLine(brief);
+        }
+    }
+}
---
+++ BearProgramPL/TeddyBear.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearProgramPL
+{
+    class TeddyBear : ToyBear
+    {
+        void ToyBear.Hug()
+        {
+            string brief = "Here is a hug";
+            Console.WriteLine(brief);
+        }
+    }
+}
